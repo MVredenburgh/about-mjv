72bac8bf88d08bf5615d2d237c0779d665de6fa3
OskiLover <michaelvredenburgh@gmail.com>
2015-07-28 15:34:56 -0700

    tests and test helpers
    verifying the validity of user login and logout

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+  end
+
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
@@ -11,4 +15,28 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
+
+  test "login with valid information" do
+    get login_path
+    post login_path, session: { email: @user.email, password: 'password' }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name:  "Example User",
+                                            email: "user@example.com",
+                                            password:              "password",
+                                            password_confirmation: "password" }
+    end
+    assert_template 'users/show'
+    assert is_logged_in?
+  end
+
 end
