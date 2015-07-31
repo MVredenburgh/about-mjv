3f7c76b69acf64b5e8f6910b25da0912b885e248
OskiLover <michaelvredenburgh@gmail.com>
2015-07-31 16:01:48 -0700

    Moved location of some tests

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -6,39 +6,6 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
-  test "login with invalid information" do
-    get login_path
-    assert_template 'sessions/new'
-    post login_path, session: { email: "", password: "" }
-    assert_template 'sessions/new'
-    assert_not flash.empty?
-    get root_path
-    assert flash.empty?
-  end
-
-  test "login with valid information" do
-    get login_path
-    post login_path, session: { email: @user.email, password: 'password' }
-    assert_redirected_to @user
-    follow_redirect!
-    assert_template 'users/show'
-    assert_select "a[href=?]", login_path, count: 0
-    assert_select "a[href=?]", logout_path
-    assert_select "a[href=?]", user_path(@user)
-  end
-
-  test "valid signup information" do
-    get signup_path
-    assert_difference 'User.count', 1 do
-      post_via_redirect users_path, user: { name:  "Example User",
-                                            email: "user@example.com",
-                                            password:              "password",
-                                            password_confirmation: "password" }
-    end
-    assert_template 'users/show'
-    assert is_logged_in?
-  end
-
   test "login with valid information followed by logout" do
     get login_path
     post login_path, session: { email: @user.email, password: 'password' }
