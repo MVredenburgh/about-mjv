e87c94ffab8e0767f87b043e9d7485f8100fbd14
OskiLover <michaelvredenburgh@gmail.com>
2015-08-03 15:55:51 -0700

    Password resets - prior to testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
-  get 'sessions/new'
-
-  get 'users/new'
-
   root	                    'static_pages#home'
   get 'help'             => 'static_pages#help'
   get 'about'            => 'static_pages#about'
@@ -21,5 +13,5 @@ Rails.application.routes.draw do
   delete 'logout' 	 => 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
-
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
