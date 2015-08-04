bd4d52ce9406b0a68515b8660ce804efc64ed271
OskiLover <michaelvredenburgh@gmail.com>
2015-08-03 19:23:03 -0700

    Final post of the day - image upload production logic

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,5 +1,12 @@
 class PictureUploader < CarrierWave::Uploader::Base
-  storage :file
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [400, 400]
+
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
