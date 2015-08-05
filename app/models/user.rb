a1142c7d4507f260afbf13905e0924a31d1f761a
OskiLover <michaelvredenburgh@gmail.com>
2015-08-05 13:53:52 -0700

    passive relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
-  has_many :active_relationships, class_name:  "Relationship",
-                                  foreign_key: "follower_id",
-                                  dependent:   :destroy
-  has_many :following, through: :active_relationships, source: :followed
+  has_many :active_relationships,  class_name:  "Relationship",
+                                   foreign_key: "follower_id",
+                                   dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
@@ -78,10 +82,20 @@ class User < ActiveRecord::Base
     Micropost.where("user_id = ?", id)
   end
 
-  def activated?
-    self.activated
+  # Follows a user.
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
   end
-    
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end    
   private
     # Converts email to all lower-case.
     def downcase_email
